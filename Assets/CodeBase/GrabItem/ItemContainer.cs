06af9c83906846f70f1baae55d829e60213861ef
Kisel_Vladislav <d.988.1861.t@gmail.com>
2024-10-20 17:10:27 +0300

    Create Level

--- Assets/CodeBase/GrabItem/ItemContainer.cs
+++ Assets/CodeBase/GrabItem/ItemContainer.cs
@@ -8,7 +8,7 @@ namespace CodeBase.GrabLogic
         [SerializeField] private List<GrabItem> _items;
 
         public bool IsFull => _items.Count >= _itemPlacements.Count;
-        public bool IsEmpty => _items[0] == null;
+        public bool IsEmpty => _items.Count == 0;
 
         public bool TryAdd(GrabItem item)
         {
@@ -20,6 +20,12 @@ namespace CodeBase.GrabLogic
 
             return true;
         }
+        public GrabItem Get()
+        {
+            var item = _items[_items.Count - 1]; 
+            _items.RemoveAt(_items.Count - 1); 
+            return item; 
+        }
 
         private void Placement(GrabItem item)
         {
@@ -27,11 +33,5 @@ namespace CodeBase.GrabLogic
             item.gameObject.transform.localPosition = Vector3.zero;
         }
 
-        public GrabItem Get()
-        {
-            var item = _items[_items.Count - 1]; 
-            _items.RemoveAt(_items.Count - 1); 
-            return item; 
-        }
     }
 }
